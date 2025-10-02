1193a6964678a98e86cb79039b4f874b1e9de308
FrancinaldoCabral <naldo_rn@hotmail.com>
2025-10-02 02:23:03 -0300

    fix: backent to heroku

--- src/app/services/env.ts
+++ src/app/services/env.ts
@@ -1,6 +1,6 @@
 export default {
     //apiHost: 'http://localhost:5000',
-    apiHost: 'https://api.redatudo.online'
+    apiHost: 'https://obizu-c95bfa20dc69.herokuapp.com/'
 }
 
 
--- src/app/services/environment.service.ts
+++ src/app/services/environment.service.ts
@@ -5,6 +5,6 @@ import { Injectable } from "@angular/core";
 })
 export class EnvironmentService {
     wpUrl:string = 'https://obizu.online'
-    apiUrl:string = 'https://api.obizu.online' 
-    //apiUrl:string = 'https://intense-reaches-77234-7e1b0e461334.herokuapp.com' //'https://api.obizu.online' 
+    //apiUrl:string = 'https://api.obizu.online' 
+    apiUrl:string = 'https://obizu-c95bfa20dc69.herokuapp.com/' 
 }
